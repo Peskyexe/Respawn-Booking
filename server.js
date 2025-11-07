c2e5cc54227264c5a6ef5685f7ef65c4c70ab0b3
Peskyexe <tthdarklama08@gmail.com>
2025-11-07 15:55:18 +0100

    Small patches

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.get("/", async (request, response) => {
     response.send( await readFile("./public/booking.html", "utf8") );
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running on http://localhost:3000"))
+app.listen(process.env.PORT || 3000, () => console.log("Server running on http://localhost:3000. To stop the server, press Ctrl + C"))
--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,4 @@
 @echo off
 echo Starting Node.js server...
 node server.js
-echo Server running on "http://localhost:3000"
 pause
