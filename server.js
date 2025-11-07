b2f949b87e6f3930c9c02941e9d03c514cf9ac0a
Peskyexe <tthdarklama08@gmail.com>
2025-11-07 15:32:56 +0100

    Node.js optimized

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
-const { readFile, readFileSync} = require("fs");
+const { readFile } = require("fs").promises;
 
-app.get("/", (request, response) => {
-    readFile("./public/booking.html", "utf8", (error, html) => {
-        if (error) {
-            response.status(500).send("A server error occurred")
-        }
-        response.send(html);
-    })
+// app.get("/", (request, response) => {
+//     readFile("./public/booking.html", "utf8", (error, html) => {
+//         if (error) {
+//             response.status(500).send("A server error occurred")
+//         }
+//         response.send(html);
+//     })
+// });
+
+app.get("/", async (request, response) => {
+    response.send( await readFile("./public/booking.html", "utf8") );
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running on http://localhost:3000"))
